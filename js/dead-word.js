8d18bedb2642af51193cd3b67496d1db69d0d77e
哇弥陀佛 <weimingjie@Live.com>
2017-05-19 19:21:19 +0800

    加入名人名言模块

---
+++ js/dead-word.js
@@ -0,0 +1,14 @@
+//随机获取json文件中的一条数据
+function getDeadWords(){
+    $.getJSON("dead-words.json",function(data){
+        var index = Math.floor(Math.random()*(data.length));
+        console.log(index);
+        $("#deadwords").html("&nbsp; &nbsp; " + data[index].Words); 
+        $("#deadman").html("—— " + data[index].Man);
+        $("#deadwordsorigin").html("&nbsp; &nbsp; " + data[index].Wordsorigin);
+        if(data[index].Wordsorigin==""){
+            $("#deadwordsorigin").hide();
+        }
+    })  
+}
+getDeadWords();
