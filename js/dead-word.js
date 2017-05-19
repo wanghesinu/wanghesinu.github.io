fb1e9de566b5063671c5e889dafa8b9a020bb658
哇弥陀佛 <weimingjie@Live.com>
2017-05-19 19:30:58 +0800

    修复亡语链接

--- js/dead-word.js
+++ js/dead-word.js
@@ -1,6 +1,6 @@
 //随机获取json文件中的一条数据
 function getDeadWords(){
-    $.getJSON("dead-words.json",function(data){
+    $.getJSON("/dead-words.json",function(data){
         var index = Math.floor(Math.random()*(data.length));
         console.log(index);
         $("#deadwords").html("&nbsp; &nbsp; " + data[index].Words); 
